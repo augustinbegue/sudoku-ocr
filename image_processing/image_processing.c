3d7e4e6efa306dd6465f16cf1df3831711e0fd89
Augustin Bégué <augustin.begue@epita.fr>
2021-11-27 17:46:29 +0100

    digit cleaning and downscaling
    Signed-off-by: Augustin Bégué <augustin.begue@epita.fr>

--- image_processing/image_processing.c
+++ image_processing/image_processing.c
@@ -14,8 +14,8 @@
  *
  * @param maskpt image mask to extract pixel from
  * @param imagept image to process
- * @param save_mask save the mask to a separate file
- * @param mask_output_path path where the mask is to be saved
+ * @param verbose_mode
+ * @param verbose_path
  */
 void image_processing_extract_grid(Image *maskpt, Image *imagept,
     bool verbose_mode, char *verbose_path, bool gtk)
@@ -139,4 +139,272 @@ void image_processing_extract_grid(Image *maskpt, Image *imagept,
     filter_threshold(imagept);
 
     verbose_save(verbose_mode, verbose_path, "5-binarized.png", imagept);
+}
+
+/**
+ * @brief Processes the images contained in input and extracts the digits
+ * contained in the grid
+ *
+ * @param input
+ * @param verbose_mode
+ * @param verbose_path
+ */
+void image_processing_extract_digits(
+    Image *input, bool verbose_mode, char *verbose_path)
+{
+    int image_size = input->width;
+
+    // Grayscaling the image
+    filter_grayscale(input, 0);
+
+    // Blurring the image
+    double *kernel = get_gaussian_smoothing_kernel(image_size / 100, 1.5);
+    convolution(kernel, image_size / 100, input, input, false);
+    free(kernel);
+
+    verbose_save(verbose_mode, verbose_path, "9.2-blurred.png", input);
+
+    // Dilation and Erosion
+    morph(input, Dilation, image_size / 250);
+    morph(input, Erosion, image_size / 250);
+
+    verbose_save(
+        verbose_mode, verbose_path, "9.3-erosion-dilation.png", input);
+
+    // Adjusting colors
+    filter_contrast(input, 64);
+    filter_gamma(input, 255);
+
+    filter_threshold(input);
+
+    verbose_save(verbose_mode, verbose_path, "9.4-colors-adjusted.png", input);
+}
+
+void remove_cell_borders(Image *input)
+{
+    int image_size = input->width;
+    int center = image_size / 2;
+
+    int left_border_width = 0;
+    int right_border_width = 0;
+    int top_border_height = 0;
+    int bottom_border_height = 0;
+
+    Image border_sampling = clone_image(input);
+
+    // Dilate image to make borders thicker
+    morph(&border_sampling, Dilation, image_size / 10);
+
+    int border_size_threshold = image_size / 8;
+    for (int x = 0; x < image_size; x++)
+    {
+        for (int y = 0; y < image_size; y++)
+        {
+            // Potentially a border
+            if (border_sampling.pixels[x][y].r == 255)
+            {
+                int border_size = 0;
+                bool border = true;
+
+                // Test if the pixel is on a horizontal border
+                int blackpixels = 0;
+                for (int xx = 0; xx < image_size; xx++)
+                    if (border_sampling.pixels[xx][y].r == 0)
+                        blackpixels++;
+
+                if (blackpixels > border_size_threshold)
+                    border = false;
+
+                if (!border)
+                {
+                    border = true;
+
+                    // Test if the pixel is on a vertical border
+                    blackpixels = 0;
+                    for (int yy = 0; yy < image_size; yy++)
+                        if (border_sampling.pixels[x][yy].r == 0)
+                            blackpixels++;
+
+                    if (blackpixels > border_size_threshold)
+                        border = false;
+
+                    if (border)
+                    {
+                        // left/right border
+                        if (x > center)
+                        {
+                            // right border
+                            border_size = image_size - x;
+
+                            if (border_size > right_border_width)
+                                right_border_width = border_size;
+                        }
+                        else
+                        {
+                            // left border
+                            border_size = x;
+
+                            if (border_size > left_border_width)
+                                left_border_width = border_size;
+                        }
+                    }
+                }
+                else
+                {
+                    // top/bottom border
+                    if (y > center)
+                    {
+                        // bottom border
+                        border_size = image_size - y;
+
+                        if (border_size > bottom_border_height)
+                            bottom_border_height = border_size;
+                    }
+                    else
+                    {
+                        // top border
+                        border_size = y;
+
+                        if (border_size > top_border_height)
+                            top_border_height = border_size;
+                    }
+                }
+            }
+        }
+    }
+
+    free_Image(&border_sampling);
+
+    // Removing borders
+    for (int y = 0; y < image_size; y++)
+    {
+        for (int x = 0; x <= left_border_width; x++)
+        {
+            input->pixels[x][y].r = 0;
+            input->pixels[x][y].g = 0;
+            input->pixels[x][y].b = 0;
+        }
+        for (int x = image_size - right_border_width; x < image_size; x++)
+        {
+            input->pixels[x][y].r = 0;
+            input->pixels[x][y].g = 0;
+            input->pixels[x][y].b = 0;
+        }
+    }
+
+    for (int x = 0; x < image_size; x++)
+    {
+        for (int y = 0; y <= top_border_height; y++)
+        {
+            input->pixels[x][y].r = 0;
+            input->pixels[x][y].g = 0;
+            input->pixels[x][y].b = 0;
+        }
+
+        for (int y = image_size - bottom_border_height; y < image_size; y++)
+        {
+            input->pixels[x][y].r = 0;
+            input->pixels[x][y].g = 0;
+            input->pixels[x][y].b = 0;
+        }
+    }
+}
+
+/**
+ * @brief Detects the boundaries of the digit countained in the black and white
+ * image
+ *
+ * @ref Algorithm description:
+ * We start by removing the white borders on the image.
+ * Then, we go trough the image and look for the most extreme coordinates of
+ * the remaining white pixels
+ *
+ *
+ * @param input input image where the digit is located
+ * @returns the boundaries of the digit
+ */
+square image_processing_detect_digit_boundaries(Image *input)
+{
+    int image_size = input->width;
+    int center = image_size / 2;
+    int min_x = image_size;
+    int max_x = 0;
+    int min_y = image_size;
+    int max_y = 0;
+
+    // Detecting the white borders of the cells
+    remove_cell_borders(input);
+
+    bool changed = true;
+    int range = image_size / 8;
+
+    while (changed)
+    {
+        changed = false;
+
+        // Searching for the most extreme coordinates of the remaining white
+        // pixels
+        int start_x = center - range;
+        int end_x = center + range;
+        int start_y = center - range;
+        int end_y = center + range;
+
+        if (start_x < 0)
+            start_x = 0;
+        if (end_x >= image_size)
+            end_x = image_size - 1;
+
+        if (start_y < 0)
+            start_y = 0;
+        if (end_y >= image_size)
+            end_y = image_size - 1;
+
+        for (int x = start_x; x <= end_x; x++)
+        {
+            for (int y = start_y; y <= end_y; y++)
+            {
+                if (input->pixels[x][y].g == 255)
+                {
+                    if (x < min_x)
+                    {
+                        min_x = x;
+                        changed = true;
+                    }
+                    if (x > max_x)
+                    {
+                        max_x = x;
+                        changed = true;
+                    }
+                    if (y < min_y)
+                    {
+                        min_y = y;
+                        changed = true;
+                    }
+                    if (y > max_y)
+                    {
+                        max_y = y;
+                        changed = true;
+                    }
+                }
+            }
+        }
+
+        range += image_size / 50;
+    }
+
+    square boundaries;
+
+    boundaries.c1.x = min_x;
+    boundaries.c1.y = min_y;
+
+    boundaries.c2.x = max_x;
+    boundaries.c2.y = min_y;
+
+    boundaries.c3.x = max_x;
+    boundaries.c3.y = max_y;
+
+    boundaries.c4.x = min_x;
+    boundaries.c4.y = max_y;
+
+    return boundaries;
 }
--- utils/image.c
+++ utils/image.c
@@ -149,7 +149,7 @@ Image clone_image(Image *source)
     return image;
 }
 
-Image crop_image(Image *input, square *crop)
+Image *crop_image(Image *input, square *crop)
 {
     point c1 = crop->c1;
     point c2 = crop->c2;
@@ -166,60 +166,99 @@ Image crop_image(Image *input, square *crop)
         = sqrt((c4.x - c1.x) * (c4.x - c1.x) + (c4.y - c1.y) * (c4.y - c1.y));
 
     int size;
+    int smallest = 0;
     if (side1_length < side2_length)
+    {
         size = side2_length;
+        smallest = side1_length;
+    }
     else
+    {
         size = side1_length;
+        smallest = side2_length;
+    }
 
     if (side3_length > size)
         size = side3_length;
-
     if (side4_length > size)
         size = side4_length;
 
-    point start;
-    if (c1.x < c2.x)
-        start.x = c1.x;
-    else
-        start.x = c2.x;
-    if (c3.x < start.x)
-        start.x = c3.x;
-    if (c4.x < start.x)
-        start.x = c4.x;
+    int offset = (int)(size - smallest) / 2;
 
-    if (c1.y < c2.y)
-        start.y = c1.y;
-    else
-        start.y = c2.y;
-    if (c3.y < start.y)
-        start.y = c3.y;
-    if (c4.y < start.y)
-        start.y = c4.y;
+    if (offset > 0)
+        c1.x -= offset;
+
+    point start = c1;
 
     printf("   ✂️ Cropping Image - Start: (%i, %i), Size: %i\n", start.x,
         start.y, size);
 
-    Image cropped;
-    cropped.height = size;
-    cropped.width = size;
-    cropped.surface = SDL_CreateRGBSurfaceWithFormat(
-        0, size, size, 32, SDL_PIXELFORMAT_RGBA32);
-    cropped.pixels = malloc(sizeof(Pixel *) * size + 1);
-
+    Image *cropped = create_image(size, size);
     int old_x = start.x;
     for (int x = 0; x < size && old_x < input->width; x++, old_x++)
     {
-        cropped.pixels[x] = malloc(sizeof(Pixel) * size + 1);
-
         int old_y = start.y;
         for (int y = 0; y < size && old_y < input->height; y++, old_y++)
         {
             Pixel old = input->pixels[old_x][old_y];
-            cropped.pixels[x][y] = old;
+            cropped->pixels[x][y] = old;
         }
     }
 
     return cropped;
+}
+
+/**
+ * @brief Downscales the input square image to the given size.
+ *
+ * @param input
+ * @param size
+ * @return Image*
+ */
+Image *downscale_image(Image *input, int size)
+{
+    Image *downscaled = create_image(size, size);
+
+    int original_size = input->width;
+    double downscale_factor = (double)original_size / (double)size;
+
+    double x = 0;
+    for (int sx = 0; sx < size && x < original_size; sx++)
+    {
+        double y = 0;
+        for (int sy = 0; sy < size && y < original_size; sy++)
+        {
+            int color = 0;
+
+            double pnum = 0;
+            for (int f = 0; f < downscale_factor && (x + f) < original_size;
+                 f++)
+            {
+                for (int g = 0;
+                     g < downscale_factor && (y + g) < original_size; g++)
+                {
+                    int xx = x + f;
+                    int yy = y + g;
+
+                    color += input->pixels[xx][yy].r;
+
+                    pnum++;
+                }
+            }
+
+            color /= pnum;
+
+            Pixel pix = {color, color, color};
+
+            downscaled->pixels[sx][sy] = pix;
+
+            y += downscale_factor;
+        }
+
+        x += downscale_factor;
+    }
+
+    return downscaled;
 }
 
 Image SDL_Surface_to_Image(SDL_Surface *image_surface)
