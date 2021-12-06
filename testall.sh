e6578026d17c99c372f25b67db8d96cd8a735dcd
Augustin Bégué <augustin.begue@epita.fr>
2021-12-06 11:44:03 +0100

    Fixed sh corruption
    Signed-off-by: Augustin Bégué <augustin.begue@epita.fr>

--- testall.sh
+++ testall.sh
@@ -1,6 +1,5 @@
-if make ; then
-    # echo ""
-    # echo "=== 1. TESTING PROCESSING ==="
+if make; then
+
     for i in $(seq 1 6);
     do
         echo ""
@@ -8,13 +7,6 @@ if make ; then
         if ./sudoku-ocr ./assets/grids/image_0$i.jpeg -o ./assets/output/output_$i.png -v ./assets/output/output_$i; then echo ""
         else exit 1
         fi
-        
     done
-
-    # echo ""
-    # echo "=== 2. TESTING ROTATION ==="
-    # if ./main ./assets/grids/image_05.jpeg -o ./assets/output/output_5_rotated.png -v ./assets/output/output_5_rotated -r 35; then echo ""
-    # else exit 2
-    # fi
 
 fi
