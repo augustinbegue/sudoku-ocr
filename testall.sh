9055688152b485f9ace025353f6d36a8558c5729
Augustin BEGUE <augustin.begue@epita.fr>
2021-12-06 12:07:42 +0100

    revert

--- testall.sh
+++ testall.sh
@@ -1,5 +1,6 @@
-if make; then
-
+if make ; then
+    # echo ""
+    # echo "=== 1. TESTING PROCESSING ==="
     for i in $(seq 1 6);
     do
         echo ""
@@ -7,6 +8,13 @@ if make; then
         if ./sudoku-ocr ./assets/grids/image_0$i.jpeg -o ./assets/output/output_$i.png -v ./assets/output/output_$i; then echo ""
         else exit 1
         fi
+        
     done
+
+    # echo ""
+    # echo "=== 2. TESTING ROTATION ==="
+    # if ./main ./assets/grids/image_05.jpeg -o ./assets/output/output_5_rotated.png -v ./assets/output/output_5_rotated -r 35; then echo ""
+    # else exit 2
+    # fi
 
 fi
